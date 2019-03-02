4abf529ff47ff23b83e1bc6b02f32953a9499fe4
Nia <nia_lin@126.com>
2019-03-03 02:27:53 +0800

    PAT

---
+++ 1007 Maximum Subsequence Sum (25)/1007 Maximum Subsequence Sum (25).c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <limits.h>
+
+
+int main()
+{
+	int i, n, cnt, sum, head, max_sum = INT_MIN, max_head, max_tail;
+
+	scanf("%d", &cnt);
+
+	scanf("%d", &n);
+	sum = max_head = head = n;
+
+	for(i = 1; i < cnt; ++i) {
+		scanf("%d", &n);
+
+		if(sum < 0)
+			sum = head = n;
+		else
+			sum += n;
+
+		if(sum >= 0 && max_sum < sum) {
+			max_sum = sum;
+			max_head = head;
+			max_tail = n;
+		}
+	}
+
+	if(INT_MIN == max_sum) {
+		max_sum = max_head > 0 ? max_head : 0;
+		max_tail = n;
+	}
+
+	printf("%d %d %d\n", max_sum, max_head, max_tail);
+
+	return 0;
+}
